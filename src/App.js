16a8aca90da625847cebfd4fbf676094a2c08a8c
Andrew Carpenter <andrew@conscious2.com>
2018-05-21 18:17:44 +0100

    Add favicon and rel fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
@@ -14,27 +13,27 @@ class App extends Component {
         </header>
         <ul className="App-external">
           <li className="App-external-item">
-            <a href="https://github.com/ajcarpenter" target="_blank">
+            <a href="https://github.com/ajcarpenter" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faGithub} />
             </a>
           </li>
           <li className="App-external-item">
-            <a href="https://twitter.com/ajcarpenter" target="_blank">
+            <a href="https://twitter.com/ajcarpenter" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faTwitter} />
             </a>
           </li>
           <li className="App-external-item">
-            <a href="https://www.facebook.com/ajcarpenter" target="_blank">
+            <a href="https://www.facebook.com/ajcarpenter" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faFacebook} />
             </a>
           </li>
           <li className="App-external-item">
-            <a href="https://www.instagram.com/ajcarpenter/" target="_blank">
+            <a href="https://www.instagram.com/ajcarpenter/" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faInstagram} />
             </a>
           </li>
           <li className="App-external-item">
-            <a href="https://www.linkedin.com/in/andrew-carpenter-24450b14/" target="_blank">
+            <a href="https://www.linkedin.com/in/andrew-carpenter-24450b14/" target="_blank" rel="noopener noreferrer">
               <FontAwesomeIcon icon={faLinkedin} />
             </a>
           </li>
