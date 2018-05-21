ec4f06d70e27ab03e743374b7e84666e1e6f691d
Andrew Carpenter <andrew@conscious2.com>
2018-05-21 18:12:09 +0100

    Simple page with social links

--- src/App.js
+++ src/App.js
@@ -2,20 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { faGithub, faTwitter, faFacebook, faInstagram, faLinkedin } from '@fortawesome/fontawesome-free-brands'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">@ajcarpenter</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul className="App-external">
+          <li className="App-external-item">
+            <a href="https://github.com/ajcarpenter" target="_blank">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+          <li className="App-external-item">
+            <a href="https://twitter.com/ajcarpenter" target="_blank">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+          </li>
+          <li className="App-external-item">
+            <a href="https://www.facebook.com/ajcarpenter" target="_blank">
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+          </li>
+          <li className="App-external-item">
+            <a href="https://www.instagram.com/ajcarpenter/" target="_blank">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </li>
+          <li className="App-external-item">
+            <a href="https://www.linkedin.com/in/andrew-carpenter-24450b14/" target="_blank">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </li>
+        </ul>
       </div>
     );
   }
 }
+
+
+
 
 export default App;
